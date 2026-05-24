d59c6ea42c2324c7b7f678d895ecd0ef4ffe4079
Bhumika-code652 <bhumikasable0605@gmail.com>
2026-05-24 13:35:32 +0530

    Update Main.java

--- scr/Main.java
+++ scr/Main.java
@@ -1,5 +1,39 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Placement Analytics Project");
+
+        String file = "data/placements.csv";
+        String line;
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            // Skip header row
+            br.readLine();
+
+            while ((line = br.readLine()) != null) {
+
+                String[] data = line.split(",");
+
+                String company = data[0];
+                String branch = data[1];
+                String hired = data[2];
+                String packageLpa = data[3];
+
+                System.out.println("Company: " + company);
+                System.out.println("Branch: " + branch);
+                System.out.println("Students Hired: " + hired);
+                System.out.println("Average Package: " + packageLpa + " LPA");
+                System.out.println("---------------------------");
+            }
+
+            br.close();
+
+        } catch (IOException e) {
+            System.out.println("Error reading file");
+        }
     }
 }
